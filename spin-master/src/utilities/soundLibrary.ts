8f65f8aeb5dd648df09290142a4e4097f76e9970
Kimmo <39192814+bithsnake@users.noreply.github.com>
2025-06-12 12:01:50 +0200

    Update soundLibrary.ts

--- spin-master/src/utilities/soundLibrary.ts
+++ spin-master/src/utilities/soundLibrary.ts
@@ -1,16 +1,18 @@
+import { assetPath } from "./imageLibrary";
+
 export type SoundLibrary = string;
-export const sfxPick: SoundLibrary = "/public/assets/audio/sfxPick.mp3";
-export const sfxDrop: SoundLibrary = "/public/assets/audio/sfxDrop.mp3";
-export const sfxPoint: SoundLibrary = "/public/assets/audio/sfxPoints.mp3";
-export const sfxCount: SoundLibrary = "/public/assets/audio/sfxCount.mp3";
+export const sfxPick: SoundLibrary = assetPath + "audio/sfxPick.mp3";
+export const sfxDrop: SoundLibrary = assetPath + "audio/sfxDrop.mp3";
+export const sfxPoint: SoundLibrary = assetPath + "audio/sfxPoints.mp3";
+export const sfxCount: SoundLibrary = assetPath + "audio/sfxCount.mp3";
 export const sfxCashRegister: SoundLibrary =
-  "/public/assets/audio/sfxCashRegister.mp3";
+  assetPath + "audio/sfxCashRegister.mp3";
 
-export const track0: SoundLibrary = "/public/assets/audio/track0.mp3";
-export const track1: SoundLibrary = "/public/assets/audio/track1.mp3";
-export const track2: SoundLibrary = "/public/assets/audio/track2.mp3";
-export const spinmaster: SoundLibrary = "/public/assets/audio/_spinmaster.mp3";
+export const track0: SoundLibrary = assetPath + "audio/track0.mp3";
+export const track1: SoundLibrary = assetPath + "audio/track1.mp3";
+export const track2: SoundLibrary = assetPath + "audio/track2.mp3";
+export const spinmaster: SoundLibrary = assetPath + "audio/_spinmaster.mp3";
 export const trackFinishStage: SoundLibrary =
-  "/public/assets/audio/trackFinishStage.mp3";
+  assetPath + "audio/trackFinishStage.mp3";
 
-export const highScoreFanfare = "/public/assets/audio/highScoreFanfare.mp3";
+export const highScoreFanfare = assetPath + "audio/highScoreFanfare.mp3";
